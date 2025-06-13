c41b4a72fc2b99b1a65f2b5c766186d2474d2e17
jiyaaswani <156311173+jiyaaswani@users.noreply.github.com>
2025-06-13 09:49:55 +0000

    Define entities

---
+++ app/src/main/java/ticket.booking/entities/Ticket.java
@@ -0,0 +1,11 @@
+package ticket.booking.entities;
+import java.util.*;
+
+public class Ticket{
+    private String ticketID;
+    private String userID;
+    private String source;
+    private String destination;
+    private Date dateOfTravel;
+    private Train train;
+}
---
+++ app/src/main/java/ticket.booking/entities/User.java
@@ -0,0 +1,10 @@
+package ticket.booking.entities;
+import java.util.*;
+
+public class User{
+    private String name;
+    private String password;
+    private String hashPassword;
+    private List<Ticket> ticketsBooked;
+    private String userID;
+}
